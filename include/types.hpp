5943a2d272083e8608afa6aafd6d0bb54231ad59
talybin <vladimir.talybin@gmail.com>
2024-05-20 12:23:59 +0200

    Fixing custom overload of type_convert with fallback

--- include/types.hpp
+++ include/types.hpp
@@ -43,45 +43,8 @@ using field_t = std::variant<
     blob_id_t
 >;
 
-template <class T>
-inline constexpr std::string_view typeid_name()
-{
-    std::string_view sv = __PRETTY_FUNCTION__;
-    sv.remove_prefix(sv.find('=') + 2);
-    return { sv.data(), sv.find(';') };
-}
 
-#if 0
-template <class T>
-struct type_converter
-{
-    template <class F>
-    T operator()(F&&) const;
-
-    // Fallback on no match found
-    T operator()(...) const
-    { throw fb::exception("can't convert to type ") << typeid_name<T>(); }
-};
-
-template <class F, class T, class = std::enable_if_t<std::is_convertible_v<F, T>>>
-using convertible_t = F;
-
-template <class T>
-template <class F>
-T type_converter<T>::operator()(convertible_t<F, T>&& f) const
-{ return std::forward<F>(f); }
-
-
-template <class F, class = std::void_t<decltype(std::to_string(F()))>>
-using str_convertible_t = F;
-
-
-template <>
-template <class F>
-std::string type_converter<std::string>::operator()(str_convertible_t<F>&& f) const
-{ return std::to_string(f); }
-
-#else
+// Visitor with type conversion
 template <class T>
 struct type_converter
 {
@@ -89,91 +52,46 @@ struct type_converter
     std::enable_if_t<std::is_convertible_v<F, T>, T>
     operator()(F&& f) const
     { return std::forward<F>(f); }
-
-    T operator()(...) const
-    { throw fb::exception("can't convert to type ") << typeid_name<T>(); }
 };
 
-template <>
-struct type_converter<std::string>
-{
-    template <class F>
-    auto operator()(F&& f) const -> decltype(std::to_string(f))
-    { return std::to_string(f); }
-
-    std::string operator()(...) const
-    { throw fb::exception("can't convert to type std::string"); }
-};
-#endif
-
-
-
-
-/*
-template <>
-template <class F>
-auto type_converter<std::string>::operator()(F&& f) const -> decltype(std::to_string(f))
-{ return std::to_string(f); }
-*/
-
-#if 0
-template <class T>
-struct type_converter
-{
-    template <class F>
-    T hmm(F&&) const;
-/*
-    template <class F>
-    std::enable_if_t<std::is_assignable_v<T, F>, T>
-    operator()(F&& f) const
-    { return std::forward<F>(f); }
-*/
-/*
-    template <class F>
-    T operator()(F&&) const
-    { throw fb::exception("hmm"); }
-*/
-};
-
-template <class T>
-template <class F>
-std::enable_if_t<std::is_assignable_v<T, F>, T>
-type_converter<T>::hmm(F&& f) const
-{ return std::forward<F>(f); }
-#endif
-
-// Converters to string
-/*
-template <class F>
-auto type_converter<std::string>::operator()(F&& f) const -> decltype(std::to_string(f))
-{ return std::to_string(std::forward<F>(f)); }
-*/
-
-/*
-    template <class F>
-    std::enable_if_t<std::is_assignable_v<T, F>, T>
-    operator()(F&& f) const
-    { return std::forward<F>(f); }
-};
-*/
-
-/*
-// Converters to string
+// Visitor for string as resultat
 template <>
 struct type_converter<std::string>
 {
     using T = std::string;
 
+    // Can be constructed from
+    template <class F>
+    std::enable_if_t<std::is_constructible_v<T, F>, T>
+    operator()(F&& f) const
+    { return T(std::forward<F>(f)); }
+
+    // Integer and floating point types
     template <class F>
     auto operator()(F&& f) const -> decltype(std::to_string(f))
-    { return std::to_string(std::forward<F>(f)); }
-
-    template <class F>
-    std::enable_if_t<std::is_assignable_v<T, F>, T>
-    operator()(F&& f) const
-    { return std::forward<F>(f); }
+    { return std::to_string(f); }
 };
-*/
+
+
+namespace detail
+{
+    // Reflection
+    template <class T>
+    inline constexpr std::string_view type_name()
+    {
+        std::string_view sv = __PRETTY_FUNCTION__;
+        sv.remove_prefix(sv.find('=') + 2);
+        return { sv.data(), sv.find(';') };
+    }
+
+    // Helper type for the visitor
+    template <class... Ts>
+    struct overloaded : Ts... { using Ts::operator()...; };
+    // Explicit deduction guide (not needed as of C++20)
+    template <class... Ts>
+    overloaded(Ts...) -> overloaded<Ts...>;
+
+} // namespace detail
 
 // Helper methods
 // Details

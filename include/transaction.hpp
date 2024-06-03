885c4bbd349332935916dbb2cc1af0bb400c58ce
Vladimir Talybin <vladimir.talybin@gmail.com>
2024-06-03 19:58:23 +0200

    No friends needed

--- include/transaction.hpp
+++ include/transaction.hpp
@@ -9,6 +9,10 @@ struct database;
 
 struct transaction
 {
+    // Construct empty transaction and attach database later
+    transaction() noexcept = default;
+
+    // Construct with attached database
     transaction(database& db) noexcept;
 
     // Start transaction if not started
@@ -29,12 +33,6 @@ struct transaction
 private:
     struct context_t;
     std::shared_ptr<context_t> _context;
-
-    // Default transaction for database has
-    // special initialization because of
-    // cross referencing
-    friend struct database;
-    transaction() noexcept { };
 };
 
 } // namespace fb
